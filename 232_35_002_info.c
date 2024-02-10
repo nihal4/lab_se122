a0e6af1aeb46d7f218431f9cc91a5b5b60da4eb6
Nihal <sm.nihal4@gmail.com>
2024-02-10 19:51:40 +0600

    push

---
+++ 232_35_002_info.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    printf("Name\t\t:\tS. M. Nihal Ahmed\n");
+    printf("ID\t\t:\t232-35-002\n");
+    printf("Department\t:\tSWE\n");
+    printf("University\t:\tDIU\n");
+}
