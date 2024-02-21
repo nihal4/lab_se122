6adef1235308ff1e6c6f1bdf86726e6621522a16
Nihal <sm.nihal4@gmail.com>
2024-02-21 12:51:47 +0600

    done 10

---
+++ task_001/problem_10.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    float R , A;
+    const float pi = 3.1416;
+    printf("enter redius\t:");
+    scanf("%f",&R);
+    printf("the area of cricle is %.3f",pi*(R*R));
+}
