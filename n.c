f75becf06a3c9c5f5d9c7465cdc09c229b61c055
Nihal <sm.nihal4@gmail.com>
2024-04-27 12:38:02 +0600

    lab_performence

--- n.c
+++ n.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 
+
+int print_func(int i, int num){
+   return printf("%d found on index: %d\n",num,i);
+}
+
 int main()
 {
-
-     int get_num;
-
-     scanf("%d", &get_num);
-     int arr[5] = {1, 2, 3, 4, 5};
-
-     int True = 0;
-
-     for (int i = 0; i <= 5; i++)
-     {
-         if (arr[i] == get_num){
-           True = 1;
-         }
-     }
-
-     if (True == 1){
-          printf("FOUND");
-     }else{
-          printf("NOT FOUND");
-     }
+    int num,input_num;
+    int num_arr[5];
+    for (int i = 0; i<5;i++){
+        scanf("%d",&num);
+        num_arr[i] = num;
+    }
+    int true;
+    true = 0;
+    scanf("%d",&input_num);
+    for (int i = 0; i<5; i++){
+        if (num_arr[i] == input_num){
+            print_func(i,input_num);
+            true = 1;
+        }
+    }
+    if (true == 0){
+        printf("NOT FOUND");
+    }
 }
