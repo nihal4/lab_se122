08d8f32b3bcc93c40e0c52d2698fbd377d694746
Nihal <sm.nihal4@gmail.com>
2024-05-21 16:07:49 +0600

    lol

--- n.c
+++ n.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int nums[5],sum=0;
+    for(int i =0; i<5 ;i++){
+        scanf("%d",&nums[i]);
+    }
+    for(int i = 0; i<5; i++){
+        sum += nums[i];
+    }
+    printf("Agrregate:%d\nAvarage:%d\n",sum,sum/5);
+}
 
